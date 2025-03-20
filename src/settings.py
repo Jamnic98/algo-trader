dddc3a73053c762076984d392cba1657baeff1fe
Jamnic98 <jamiestimpson30@gmail.com>
2025-03-20 20:53:44 +0000

    update settings

--- src/settings.py
+++ src/settings.py
@@ -1,22 +1,24 @@
 import os
+from enum import StrEnum
 from dotenv import load_dotenv
 from src.logger import logger
 
+class Environment(StrEnum):
+    DEV = 'development'
+    PROD = 'production'
+
 load_dotenv()
 
-config = {
-    'DEBUG': True,
-    'LIVE_TRADING': False,
-    'ALPACA_KEY': os.getenv('ALPACA_PAPER_API_KEY'),
-    'ALPACA_SECRET': os.getenv('ALPACA_PAPER_API_SECRET'),
-    'DATABASE_URI': os.getenv('DEV_DB_URI')
-}
+config = {}
 
-environment = os.getenv('ENV', 'development')
-if environment == 'development':
-    pass
-elif environment == 'production':
-    # Production environment settings
+environment = os.getenv('ENV', Environment.DEV.value)
+if environment == Environment.DEV.value:
+    config['DEBUG'] = True
+    config['LIVE_TRADING'] = False
+    config['ALPACA_KEY'] = os.getenv('ALPACA_PAPER_API_KEY')
+    config['ALPACA_SECRET'] = os.getenv('ALPACA_PAPER_API_SECRET')
+    config['DATABASE_URI'] = os.getenv('DEV_DB_URI')
+elif environment == Environment.PROD.value:
     config['DEBUG'] = False
     config['LIVE_TRADING'] = True
     config['ALPACA_KEY'] = os.getenv('ALPACA_LIVE_API_KEY')
