2e4f3454f5ac2b2e1306a1d9d53233c011722304
Jamie Stimpson <jamiestimpson30@gmail.com>
2026-02-24 16:58:45 +0000

    Fix dashboard build

--- dashboard/src/components/index.ts
+++ dashboard/src/components/index.ts
@@ -1,4 +1,2 @@
-export { default as BotsOverview } from './BotsOverview'
-export { default as TradesOverview } from './TradesOverview'
 export { default as Footer } from './Footer'
 export { default as Header } from './Header'
